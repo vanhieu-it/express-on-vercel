cf46124dcad007e898a02edee1ad216bad286284
Văn Hiếu <hieu.dovan@thegioididong.com>
2024-01-05 13:33:07 +0700

    up cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const tinNhanRoutes = require('./src/routes/tinNhanRoutes');
 const pool = require('./src/db');
 require('dotenv').config();
@@ -8,6 +9,14 @@ const port = process.env.PORT || 3000;
 
 // Kết nối đến cơ sở dữ liệu PostgreSQL
 pool.connect();
+app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',  // Thay đổi domain tùy theo ứng dụng của bạn
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
